0d6b25858ec0e65f99b718102b29cd2b5cdee1f7
averagetoaster <88263860+sharonxrzhu@users.noreply.github.com>
2022-08-05 21:06:55 -0700

    Create python.py

---
+++ python.py
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Hello!</title>
+        <style>
+            h1{
+                color: blue;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to my website!</h1>
+        <h1>Welcome to my website!!</h1>
+        Hello, World!
+        <h2>Hello</h2>
+        <!-- git clone <git link> = download git repository to own computer(make sure first cd to the write folder)
+        git add <filename> =  add files to be committed
+        git commit -m "message" = commit with a message 
+        git status = check status
+        git push = push to github
+        git commit -am "message" = add all files that have been changed and commit all of them
+        git pull = download the latest changes
+        
+        Merge Conflict
+        -->
+    </body>
+</html>
